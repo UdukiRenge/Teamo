ecd65367eab8a51d5723de7c9f6445cfb4dee5a4
Ren Furusato <renrendk1031@gmail.com>
2025-09-06 23:25:56 +0900

    トークン生成修正

--- teamo-backend/pages/api/users/logout.js
+++ teamo-backend/pages/api/users/logout.js
@@ -6,8 +6,8 @@ async function handler(request, response) {
     try {
       // Cookieをクリア
       response.setHeader('Set-Cookie', [
-        'access_token=; HttpOnly; Path=/; Secure; Max-Age=0',
-        'refresh_token=; HttpOnly; Path=/; Secure; Max-Age=0',
+        'access_token=; HttpOnly; Path=/; Secure; Max-Age=0 SameSite=None' ,
+        'refresh_token=; HttpOnly; Path=/; Secure; Max-Age=0 SameSite=None',
       ]);
 
       return response.status(200).json({
